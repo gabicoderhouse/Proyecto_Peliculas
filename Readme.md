6002f3f8008509f7f028a25361033af125559cfa
Gerardo A Deluca <gerardo.deluca@gurusoluciones.com>
2022-10-13 18:40:05 -0300

    version final antes del pull

---
+++ Readme.md
@@ -0,0 +1,14 @@
+Una vez descargado el proyecto desde github seguimos los siguientes pasos:
+. Instalar el entorno virtual venv como venv
+. habilitar el entorno virtual
+. instalar lo detallado en el archivo requirements.txt
+. isntalar una base Sqlite3 (migration)
+. iniciar el servicio de django
+. en la dirección base inicia la página principal
+. Desde el link de "Ver Peliculas" se inicia la navegación del Blog,
+  permitiendo: Ingresar, revisar y eliminar el detalle de películas cargados.
+  También se pueden realizar busquedas por Nombre completo o parte del mismo.
+
+  Solo está habilitado el link "Acerca de" con un texto estático.
+  
+
--- home/views.py
+++ home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from home.models import Pelicula
 from home.forms import FormPelicula, BusquedaPelicula
 
@@ -17,4 +17,75 @@ def ver_peliculas(request):
     
     formulario = BusquedaPelicula()
     
-    return render(request, 'home/ver_peliculas.html', {'pelculas': peliculas, 'formulario':formulario})
+    return render(request, 'home/ver_peliculas.html', {'peliculas': peliculas, 'formulario':formulario})
+
+def crear_pelicula(request):
+    
+    if request.method == 'POST':
+        
+        formulario = FormPelicula(request.POST)
+        
+        if formulario.is_valid():
+            data = formulario.cleaned_data
+            
+            nombre = data['nombre']
+            genero = data['genero']
+            resumen = data['resumen']
+            anio_estreno = data['anio_estreno']
+            duracion = data['duracion']
+            clasificacion_edad = data['clasificacion_edad']
+            
+            pelicula = Pelicula(nombre=nombre, 
+                                genero=genero, 
+                                resumen=resumen, 
+                                anio_estreno=anio_estreno,
+                                duracion=duracion,
+                                clasificacion_edad=clasificacion_edad)
+            pelicula.save()
+            
+            return redirect('ver_peliculas')
+    
+    formulario = FormPelicula()
+    
+    return render(request, 'home/crear_pelicula.html', {'formulario': formulario})
+
+def editar_pelicula(request, id):
+    
+    pelicula = Pelicula.objects.get(id=id)
+    
+    if request.method == 'POST':
+        formulario = FormPelicula(request.POST)
+        
+        if formulario.is_valid():
+            datos = formulario.cleaned_data
+            
+            pelicula.nombre = datos['nombre']
+            pelicula.genero = datos['genero']
+            pelicula.resumen = datos['resumen']
+            pelicula.anio_estreno = datos['anio_estreno']
+            pelicula.duracion = datos['duracion']
+            pelicula.clasificacion_edad = datos['clasificacion_edad']
+            pelicula.save()
+            
+            return redirect('ver_peliculas')
+        else:
+            return render(request, 'home/editar_pelicula.html', {'formulario': formulario})
+    
+    formulario = FormPelicula(
+        initial={
+            'nombre': pelicula.nombre,
+            'genero': pelicula.genero,
+            'resumen': pelicula.resumen,
+            'anio_estreno': pelicula.anio_estreno,
+            'duracion' : pelicula.duracion,
+            'clasificacion_edad' : pelicula.clasificacion_edad
+        }
+    )
+    
+    return render(request, 'home/editar_pelicula.html', {'formulario': formulario, 'pelicula': pelicula})
+    
+def eliminar_pelicula(request, id):
+    pelicula = Pelicula.objects.get(id=id)
+    pelicula.delete()
+    return redirect('ver_peliculas')
+            
