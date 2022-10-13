c8874cb8900d84f55a1e58f9f215fa05b93e397b
Gerardo A Deluca <gerardo.deluca@gurusoluciones.com>
2022-10-13 14:31:28 -0300

    Subo el forms

---
+++ home/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+class FormPelicula(forms.Form):
+    nombre = forms.CharField(max_length=60, label='Nombre')
+    genero = forms.CharField(max_length=30, label='Género')
+    resumen = forms.CharField(max_length=200, label='Resumen')
+    anio_estreno = forms.IntegerField(label='Año Estreno')    
+    duracion = forms.IntegerField(label='Duración(seg)')
+    clasificacion_edad = forms.IntegerField(label='Clasificación') 
+    
+    
+class BusquedaPelicula(forms.Form):
+    nombre = forms.CharField(max_length=60, required=False)
