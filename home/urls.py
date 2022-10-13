9c4be2ff44a3f5dce00f92f9e976fd0c3b064242
Gerardo A Deluca <gerardo.deluca@gurusoluciones.com>
2022-10-13 15:13:59 -0300

    Actualizo base.html views urls

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from home import views
 
 urlpatterns = [
-    path('', views.index),
-    path('hola-mundo', views.hola_mundo),
+    path('', views.index, name='index'),
+    path('ver_peliculas/', views.ver_peliculas, name= 'ver_peliculas'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
-#from home.models import Familiar
+from home.models import Pelicula
+from home.forms import FormPelicula, BusquedaPelicula
 
-def hola_mundo(request):
-    return render(request, 'home/hola.html' )
 
 def index(request):
     return render(request, 'home/index.html')
+
+def ver_peliculas(request):
+    
+    nombre = request.GET.get('nombre', None)
+    
+    if nombre:
+        peliculas = Pelicula.objects.filter(nombre__icontains=nombre)
+    else:
+        peliculas = Pelicula.objects.all()
+    
+    formulario = BusquedaPelicula()
+    
+    return render(request, 'home/ver_peliculas.html', {'pelculas': peliculas, 'formulario':formulario})
