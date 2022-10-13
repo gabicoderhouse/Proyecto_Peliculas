442835654031c8330b6819d66e342656ed0e0f66
Gerardo A Deluca <gerardo.deluca@gurusoluciones.com>
2022-10-13 18:40:54 -0300

    version final antes del pull 2

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,7 @@ from home import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('ver_peliculas/', views.ver_peliculas, name= 'ver_peliculas'),
+    path('crear_pelicula/', views.crear_pelicula, name= 'crear_pelicula'),
+    path('ver_peliculas/editar_pelicula/<int:id>', views.editar_pelicula, name= 'editar_pelicula'),
+    path('ver_peliculas/eliminar_pelicula/<int:id>', views.eliminar_pelicula, name= 'eliminar_pelicula')
 ]
